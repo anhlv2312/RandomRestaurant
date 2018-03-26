55739a7e45c33466a5ad0d1a0fb4dd751210551c
Vu Anh Le <anhlv@ymail.com>
2018-03-27 00:07:42 +1000

    load dishes ok

--- index.php
+++ index.php
@@ -7,33 +7,50 @@
 	<meta charset="utf-8">
 	<title>Random Restaurant</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="js/jquery.min.js"></script>
 	<script type="text/javascript">
-	$(document).ready(function(){
-		$.getJSON('data.php?action=get_categories', function(data){
-            $.each(data, function(i, item){
-                $('.category-menu').append('<li><a href="">' + item.cat_name + '</a></li>');
-                console.log(item);
-            })
-        });
-	});
-	</script>
+		function cleanString(string){
+			result = string.replace(/[^a-zA-Z ]/g, "").replace(/\s+/g, "_").toLowerCase();
+			return result;
+		};
+		$(document).ready(function(categories){
+			$.getJSON('data.php?action=get_categories', function(data){
+				data.forEach(function(item){
+					$('ul.category-menu').append('<li>' + item.cat_name + '</li>');
+					$('article.main-menu').append('<div id="' + cleanString(item.cat_name) + '"></div>');
+				});
+				$('article.main-menu div').hide();
+				$('article.main-menu div:first-of-type').show();
+			});
+			$.getJSON('data.php?action=get_dishes', function(data){
+				data.forEach(function(item){
+					$('article.main-menu div#' + cleanString(item.cat_name)).append('<p><strong>' + item['name'] + '</strong></p><p>' + item['description'] + '</p>');
+				});
+			});
 
+		});
+		$(document).on('click', 'ul.category-menu li', function(){
+			$('ul.category-menu li').removeClass('selected')
+			$(this).addClass('selected');
+			$('article.main-menu div').hide();
+			$('article.main-menu div#' + cleanString($(this).text())).show();
+		});
+	</script>
 </head>
 <body>
 	<nav>
 		<h3 class="brand-name" >
 			<a href="#">
 				<span style="color: #ca661a">Random</span>
-				<span style="color: #ca3415"> Restaurant</span>
+				<span style="color: #ca3415">Restaurant</span>
 			</a>
 		</h3>
-		<ul class="main-menu">
-			<li><a href="#">Order</a></li> 
-			<li><a href="#">View</a></li> 
-			<li><a href="#">About</a></li> 
+		<ul class="nav-bar">
+			<li><a href="">Home</a></li> 
+			<li><a href="">Order Takeaway</a></li> 
+			<li><a href="">Where we are</a></li>
+			<li><a href="">About us</a></li>
 			<?php if (isset($username)): ?>
 				<li><a href="logout.php">Logout</a></li>
 			<?php else: ?>
@@ -43,38 +60,46 @@
 	</nav>
 
 	<header>
-		<h1 class="banner"></h1>
+		<h1 class="page-title">Takeaway</h1>
 	</header>
 
 	<main>
-
-		<aside>
-			<h3>OrderDetail</h3>
-		</aside>
+		<?php if (isset($username)): ?>
+			<aside class="order-detail">
+				<h2 class="order-detail">OrderDetail</h2>
+			</aside>
+		<?php endif ?>
 
 		<section>
-            <ul class="category-menu">
+			<ul class="category-menu"></ul>
+			<article class="main-menu">
 
-            </ul>
+			</article>
 		</section>
+
 	</main>
 
 	<footer>
-		<ul>
-			<li>first</li>
-			<li>second</li>
-			<li>third</li>
-		</ul>
-		<ul>
-			<li>first</li>
-			<li>second</li>
-			<li>third</li>
-		</ul>
-		<ul>
-			<li>first</li>
-			<li>second</li>
-			<li>third</li>
-		</ul>
+		<article class="footer">
+			<h4>Location</h4>
+			<p>23 Random Street, St. Lucia, QLD 4067</p>
+		</article>
+		<article class="footer">
+			<h4>Open hours</h4>
+			<p>Lunch: 12:00pm to 3:00pm, Monday to Friday</p>
+			<p>Dinner: 4:30pm to 10:30pm, Monday to Sunday</p>
+		</article>
+		<article class="footer">
+			<h4>Contact</h4>
+			<p>+61 450 087 209</p>
+			<p>vuanh.le@uqconnect.edu.au</p>
+		</article>
+		<h3 class="brand-name" >
+			<a href="#">
+				<span style="color: #ca661a">Random</span>
+				<span style="color: #ca3415"> Restaurant</span>
+			</a>
+		</h3>
 	</footer>
 
 </body>
