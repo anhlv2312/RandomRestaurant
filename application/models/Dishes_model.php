c4e7ae7122796598d7657e93eeaf126ec47a3c1f
Vu Anh Le <anhlv@ymail.com>
2018-04-15 20:52:59 +1000

    reorder catetory by priority

--- application/models/Dishes_model.php
+++ application/models/Dishes_model.php
@@ -6,11 +6,13 @@ class Dishes_model extends CI_Model {
 	}
 
 	public function get_categories() {
+		$this->db->order_by("priority");
 		$query = $this->db->get('categories');
 		return $query->result();
 	}
 
 	public function get_dishes() {
+		$this->db->order_by("dish_code");
 		$query = $this->db->get('dishes');
 		return $query->result();
 	}
