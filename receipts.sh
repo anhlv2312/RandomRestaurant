2a6e4f53b3e686ec7785cdfcb0526bb304062237
Vu Anh Le <anhlv@ymail.com>
2018-05-21 21:57:38 +1000

    mistyping

--- receipts.sh
+++ receipts.sh
@@ -1,2 +1,2 @@
 rm -rf ./receipts/temp.pdf
-/usr/local/bin/wkhtmltopdf --quite --dpi 300 --page-size A6 "$1" ./receipts/temp.pdf
+/usr/local/bin/wkhtmltopdf --quiet --dpi 300 --page-size A6 "$1" ./receipts/temp.pdf
