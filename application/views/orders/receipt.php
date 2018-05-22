b05fd346402c8a92ab11be495c1e656f3f25cfa2
Vu Anh Le <anhlv@ymail.com>
2018-05-22 12:02:55 +1000

    Receipt formated

--- application/views/orders/receipt.php
+++ application/views/orders/receipt.php
@@ -8,24 +8,30 @@
 	<script src="<?php echo base_url('js/jquery.min.js') ?>"></script>
 </head>
 <body id="receipt">
+	<h2 class="logo" >
+		<span style="color: #ca661a">Random</span>
+		<span style="color: #ca3415">Restaurant</span>
+	</h2>
+	<p>https://www.randomrestaurant.xyz</p>
+	<p>23 Random Street, St. Lucia, QLD 4067</p>
+
+	<h3>Order Receipt</h3>
+	<ul>
+	<li>Customer Number: <strong><?php echo $order['user_id'] ?></strong></li>
+	<li>Order Time: <strong><?php echo $order['order_time'] ?></strong></li>
+	<li>Order Number: <strong><?php echo $order['order_id'] ?></strong></li>
+	</ul>
 	<table>
 		<?php $total = 0; ?>
 		<?php foreach ($order['items'] as $item): ?>
 			<tr>
-				<td><?php echo $item['quantity'] ?> x <?php echo $item['dish_code'] ?>.</td>
-				<td><?php echo $item['name'] ?></td>
+				<td><?php echo $item['quantity'] ?> x </td><td><?php echo $item['dish_code'] ?>. <?php echo $item['name'] ?></td>
 				<td>$<?php echo number_format($item['price'] * $item['quantity'], 2)?></td>
 			</tr>
 			<?php $total += $item['price'] * $item['quantity']; ?>
 		<?php endforeach; ?>
-		<tr class="total">
+		<tr>
 			<td colspan="3">Total Amount: <strong>$<?php echo number_format($total, 2) ?></strong></td>
-		</tr>
-		<tr class="time">
-			<td colspan="3">Order Number: <strong><?php echo $order['order_id'] ?></strong></td>
-		</tr>
-		<tr class="time">
-			<td colspan="3">Order Time: <?php echo $order['order_time'] ?></td>
 		</tr>
 	</table>
 </body>
